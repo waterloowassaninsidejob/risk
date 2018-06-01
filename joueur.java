371b968be246981f32c71be5b69af513880c40c5
G6C <33627534+MarineDaCosta@users.noreply.github.com>
2018-06-01 15:30:53 +0200

    Update joueur.java

--- joueur.java
+++ joueur.java
@@ -1,1 +1,11 @@
+import java.util.Vector;
 
+public class Joueur {
+
+	private int id;
+	private String nom;
+	private Mission mission;
+	private int armee;
+	private Vector<Territoire> territoire;
+	private Vector<Region> region;
+	private Vector<Unite> unite;

995fd14c466296b5e5bb7c517c6eaa58e2903742
waterloowassaninsidejob <33628702+waterloowassaninsidejob@users.noreply.github.com>
2018-06-03 16:30:16 +0200

    Update main.java

--- main.java
+++ main.java
@@ -68,167 +68,54 @@ public class main {
 			StdDraw.picture(500, 500, "image/carte.png");
 			StdDraw.text(500, 100, "J1 veuillez regarder votre mission et placer vos armées");
 			int choix3 = 1;
+			StdDraw.setPenColor(178,190,181);
+			StdDraw.filledCircle(68, 885, 20);//Alaska
+			StdDraw.filledCircle(140, 885, 20);//Canada
+			StdDraw.filledCircle(125, 815, 20);//Montana
+			StdDraw.filledCircle(190, 810, 20);//Ontario
+			StdDraw.filledCircle(255, 805, 15);//Québec
+			StdDraw.filledCircle(240, 185, 15);//Argentine
+			StdDraw.filledCircle(237, 307, 15);//Pérou
+			StdDraw.filledCircle(284, 340, 20);//Brésil
+			StdDraw.filledCircle(223, 466, 20);//Vénézuela
+			StdDraw.filledCircle(530, 274, 20);//Afrique du Sud
+			StdDraw.filledCircle(610, 281, 15);//Madagascar
+			StdDraw.filledCircle(530, 414, 20);//Congo
+			StdDraw.filledCircle(582, 480, 20);//Ethiopie
+			StdDraw.filledCircle(450, 548, 20);//Sahara
+			StdDraw.filledCircle(530, 614, 20);//Egypte
+			StdDraw.filledCircle(461, 739, 15);//France
+			StdDraw.filledCircle(850,222,20);//Perth
+			StdDraw.filledCircle(930,229,18);//Sydney
+			StdDraw.filledCircle(934,380,15);//Nouvelle Calédonie
+			StdDraw.filledCircle(840,422,15);//Indonésie
+			StdDraw.filledCircle(776,659,20);//Chine
+			StdDraw.filledCircle(719,585,20);//Inde
+			StdDraw.filledCircle(588,669,20);//Turquie
+			StdDraw.filledCircle(907,681,15);//Japon
+			StdDraw.filledCircle(784,740,20);//Mongolie
+			StdDraw.filledCircle(776,800,20);//Irkusta
+			StdDraw.filledCircle(885,874,20);//Kamchatka
+			StdDraw.filledCircle(792,884,20);//Yakust
+			StdDraw.filledCircle(712,877,20);//Sibérie
+			StdDraw.filledCircle(654,844,20);//Ural
+			StdDraw.filledCircle(662,741,20);//Afghanistan
+			StdDraw.filledCircle(526,731,15);//Grèce
+			StdDraw.filledCircle(431,800,15);//Angleterre
+			StdDraw.filledCircle(412,862,15);//Islande
+			StdDraw.filledCircle(504,867,15);//Scandinavie
+			StdDraw.filledCircle(569,807,20);//Russie
+			StdDraw.filledCircle(500,793,15);//Allemagne
+			StdDraw.filledCircle(100,730,20);//West
+			StdDraw.filledCircle(180,700,20);//East
+			StdDraw.filledCircle(110,600,20);//Mexique
+			StdDraw.filledCircle(360,930,20);//Groënland
+			StdDraw.filledCircle(800,542,15);//Thaïlande
+
 			while (choix3 != 0) {
 				if (StdDraw.isMousePressed()) {
 					double i = StdDraw.mouseX();
 					double j = StdDraw.mouseY();
-					if (60 < i && 80 > i && 880 < j && 900 > j) {
-
-						StdDraw.text(500, 200, "Alaska");
-						break;
-					}
-					if (130 < i && 150 > i && 880 < j && 900 > j) {
-
-						StdDraw.text(500, 200, "Canada");
-						break;
-					}
-					if (115 < i && 135 > i && 800 < j && 830 > j) {
-
-						StdDraw.text(500, 200, "Montana");
-						break;
-					}
-					if (175 < i && 195 > i && 800 < j && 830 > j) {
-
-						StdDraw.text(500, 200, "Ontario");
-						break;
-					}
-					if (230 < i && 250 > i && 800 < j && 830 > j) {
-
-						StdDraw.text(500, 200, "Québec");
-						break;
-					}
-					if (220 < i && 240 > i && 168 < j && 188 > j) {
-
-						StdDraw.text(500, 200, "Argentine");
-						break;
-					}
-					if (227 < i && 247 > i && 297 < j && 317 > j) {
-
-						StdDraw.text(500, 200, "Pérou");
-						break;
-					}
-					if (274 < i && 294 > i && 330 < j && 350 > j) {
-						StdDraw.text(500, 200, "Brésil");
-						break;
-					}
-					if (213 < i && 233 > i && 456 < j && 476 > j) {
-						
-						StdDraw.text(500, 200, "Vénézuela");
-						break;
-					}
-					if (520 < i && 540 > i && 264 < j && 284 > j) {
-						
-						StdDraw.text(500, 200, "Afrique du Sud");
-						break;
-					}
-					if (605 < i && 625 > i && 271 < j && 291 > j) {
-						
-						StdDraw.text(500, 200, "Madagascar");
-						break;
-					}
-					if (520 < i && 540 > i && 404 < j && 424 > j) {
-						StdDraw.text(500, 200, "Congo");
-						break;
-					}
-					if (572 < i && 592 > i && 476 < j && 499 > j) {
-						StdDraw.text(500, 200, "Ethiopie");
-						break;
-					}
-					if (440 < i && 460 > i && 538 < j && 558 > j) {
-						StdDraw.text(500, 200, "Sahara");
-						break;
-					}
-					if (520 < i && 540 > i && 604 < j && 624 > j) {
-						StdDraw.text(500, 200, "Egypte");
-						break;
-					}
-					if (451 < i && 471 > i && 723 < j && 743 > j) {
-						StdDraw.text(500, 200, "France");
-						break;
-					}
-					if (840 < i && 860 > i && 212 < j && 232 > j) {
-						StdDraw.text(500, 200, "Perth");
-						break;
-					}
-					if (920 < i && 940 > i && 219 < j && 239 > j) {
-						StdDraw.text(500, 200, "Sydney");
-						break;
-					}
-					if (924 < i && 944 > i && 360 < j && 380 > j) {	
-						StdDraw.text(500, 200, "Nouvelle Calédonie");
-						break;
-					}
-					if (830 < i && 850 > i && 412 < j && 432 > j) {	
-						StdDraw.text(500, 200, "Indonésie");
-						break;
-					}if (766 < i && 786 > i && 649 < j && 669 > j) {	
-						StdDraw.text(500, 200, "Chine");
-						break;
-					}
-					if (709 < i && 729 > i && 575 < j && 595 > j) {	
-						StdDraw.text(500, 200, "Inde");
-						break;
-					}
-					if (573 < i && 593 > i && 669 < j && 689 > j) {	
-						StdDraw.text(500, 200, "Turquie");
-						break;
-					}
-					if (897 < i && 917 > i && 671 < j && 691 > j) {						
-						StdDraw.text(500, 200, "Japon");
-						break;
-					}
-					if (774 < i && 794 > i && 730 < j && 750 > j) {						
-						StdDraw.text(500, 200, "Mongolie");
-						break;
-					}
-					if (766 < i && 786 > i && 790 < j && 810 > j) {	
-						StdDraw.text(500, 200, "Irkusta");
-						break;
-					}
-					if (875 < i && 895 > i && 864 < j && 884 > j) {	
-						StdDraw.text(500, 200, "Kamchatka");
-						break;
-					}
-					if (782 < i && 802 > i && 864 < j && 884 > j) {	
-						StdDraw.text(500, 200, "Yakust");
-						break;
-					}
-					if (702 < i && 722 > i && 857 < j && 877 > j) {
-						StdDraw.text(500, 200, "Sibérie");
-						break;
-					}
-					if (644 < i && 664 > i && 834 < j && 854 > j) {	
-						StdDraw.text(500, 200, "Ural");
-						break;
-					}
-					if (652 < i && 672 > i && 731 < j && 751 > j) {	
-						StdDraw.text(500, 200, "Afghanistan");
-						break;
-					}
-					if (521 < i && 541 > i && 716 < j && 736 > j) {	
-						StdDraw.text(500, 200, "Grèce");
-						break;
-					}
-					if (421 < i && 441 > i && 790 < j && 810 > j) {	
-						StdDraw.text(500, 200, "Angleterre");
-						break;
-					}
-					if (402 < i && 422 > i && 842 < j && 862 > j) {	
-						StdDraw.text(500, 200, "Islande");
-						break;
-					}
-					if (494 < i && 514 > i && 857 < j && 877 > j) {	
-						StdDraw.text(500, 200, "Scandinavie");
-						break;
-					}
-					if (559 < i && 579 > i && 797 < j && 817 > j) {	
-						StdDraw.text(500, 200, "Russie");
-						break;
-					}
-					if (490 < i && 510 > i && 783 < j && 803 > j) {	
-						StdDraw.text(500, 200, "Allemagne");
-						break;
-					}
 				}
 			}
 			break;

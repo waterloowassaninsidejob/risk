817d598173e3bcd1ac1782e636ead070298c887e
waterloowassaninsidejob <33628702+waterloowassaninsidejob@users.noreply.github.com>
2018-06-08 09:37:17 +0200

    Update carte.java

--- carte.java
+++ carte.java
@@ -1,1 +1,17 @@
 
+public class carte {
+	public int habitant;
+	public carte(int hab) {
+		this.habitant=hab;
+	}
+	public int getHabitant() {
+		return habitant;
+	}
+	public void setHabitant(int habitant) {
+		this.habitant = habitant;
+	}
+	public void nbrhab(int habitant) {
+		
+	}
+}
+
